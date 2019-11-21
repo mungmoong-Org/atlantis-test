c486e2a5492b8b675f9e84425064552e5c86cc7f
root <root@ip-10-10-111-246.ap-northeast-2.compute.internal>
2019-11-21 05:11:39 +0000

    add backend

---
+++ remote.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    encrypt        = "true"
+    bucket         = "atlantis-test-bucket"
+    key            = "terraform/aws/doe-dev/atlantis-test/terraform.tfstate"
+    region         = "eu-west-1"
+  }
+}
+
