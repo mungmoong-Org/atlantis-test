f25cbbefbf5559da0f2d7b2e61c40a8dd00836d7
root <root@ip-10-10-111-246.ap-northeast-2.compute.internal>
2019-11-21 05:23:28 +0000

    aa

--- remote.tf
+++ remote.tf
@@ -3,7 +3,7 @@ terraform {
     encrypt        = "true"
     bucket         = "atlantis-test-bucket"
     key            = "terraform/aws/doe-dev/atlantis-test/terraform.tfstate"
-    region         = "ap-northeast-1"
+    region         = "ap-northeast-2"
   }
 }
 
